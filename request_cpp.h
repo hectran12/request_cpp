50b3e5393105fbedc54736351dcd70eb57c4454b
tronghoa2008 <86024288+hectran12@users.noreply.github.com>
2025-01-13 03:19:21 +0700

    header file
    ...some function has been added

---
+++ request_cpp.h
@@ -0,0 +1,71 @@
+#ifndef REQUEST_CPP_H
+#define REQUEST_CPP_H
+
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class validationException : public exception
+{
+public:
+	const char* what() const throw()
+	{
+		return "Invalid URL -> url must startwith http:// or https://";
+	}
+
+};
+
+class execException : public exception
+{
+public:
+	const char* what() const throw()
+	{
+		return "Error when execute command";
+	}
+};
+
+
+class needMoreInfoException : public exception
+{
+public:
+	const char* what() const throw()
+	{
+		return "Need more information (maybe lost url or method)";
+	}
+
+};
+
+
+class request {
+public:
+	string url; // define url as a string
+	string method; // define method request as a string (includes get, post, put, delete, etc...)
+	bool silent; // define silent as a boolean (true or false)
+
+	// main constructor (initialize url and method, ...)
+	request(string url, string method);
+
+	// exec to cmd
+	string execToCMD(const string& command);
+
+	// check url is valid
+	bool checkValidUrl(string url);
+
+	// set url to the request
+	void setUrl(string url);
+
+	// set method to the request
+	void setMethod(string method);
+
+	// set silent to the exec command
+	void setSilent(bool silent);
+
+
+	// action simple_get 
+	string simple_get();
+};
+
+
+#endif

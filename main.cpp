c4beb7da61e73826ca838f0b1496ef933afcd480
tronghoa2008 <86024288+hectran12@users.noreply.github.com>
2025-01-13 23:55:01 +0700

    Update main.cpp
    optimize code

--- main.cpp
+++ main.cpp
@@ -9,14 +9,9 @@ void outBar(int amount)
 	cout << endl;
 }
 
-int main()
+
+void test_case_1()
 {
-
-
-	// test valid url
-
-
-	
 	cout << "Test case 1: valid url" << endl;
 	try {
 		request obj("hello", "get");
@@ -24,17 +19,24 @@ int main()
 	catch (exception& e) {
 		cout << e.what() << endl;
 	}
-	
-	
-	outBar(100);
 
+	outBar(100);
+}
+
+
+void test_case_2()
+{
 	cout << "Test case 2: show properties" << endl;
 	request req("https://kiemtraip.com", "get");
 	cout << "url: " << req.url << endl;
 	cout << "method: " << req.method << endl;
 
 	outBar(100);
+}
 
+void test_case_3()
+{
+	request req("https://kiemtraip.com", "get");
 	cout << "Test case 3: test set properties (url)" << endl;
 	try {
 		req.setUrl("hello");
@@ -42,18 +44,22 @@ int main()
 	catch (exception& e) {
 		cout << e.what() << endl;
 	}
-
 	outBar(100);
+}
 
+void test_case_4()
+{
+	request req("https://kiemtraip.com", "get");
 	cout << "Test case 4: test set properties" << endl;
 	req.setUrl("https://google.com");
 	req.setMethod("get");
-
 	cout << "url: " << req.url << endl;
 	cout << "method: " << req.method << endl;
-
-
 	outBar(100);
+}
+
+void test_case_5()
+{
 	/*
 		cout << "Test case 5: test execToCMD" << endl;
 		string command = "curl -X GET https://kiemtraip.com";
@@ -65,7 +71,11 @@ int main()
 		}
 
 	*/
+}
 
+
+void test_case_6()
+{
 	cout << "Test case 6: test simple_get" << endl;
 	request obj2("https://kiemtraip.com/raw.php", "get");
 
@@ -76,6 +86,44 @@ int main()
 	catch (exception& e) {
 		cout << e.what() << endl;
 	}
+
+
+	outBar(100);
+}
+
+
+void test_case_7()
+{
+	try
+	{
+		request req("https://kiemtraip.com", "get");
+
+		cout << "Timeout of request: " << req.timeout << endl;
+
+		req.setTimeout(120); // increasing timeout to 120s
+
+		cout << "Timeout of request (after set): " << req.timeout << endl;
+
+	}
+	catch (exception& e)
+	{
+		cout << e.what() << endl;
+	}
+
+	outBar(100);
+}
+
+int main()
+{
+
+
+	test_case_1();
+	test_case_2();
+	test_case_3();
+	test_case_4();
+	test_case_5();
+	test_case_6();
+	test_case_7();
 	
 
 

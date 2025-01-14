27f45004fca1d1bec439deca198543cecd50a8fc
tronghoa2008 <86024288+hectran12@users.noreply.github.com>
2025-01-14 14:35:20 +0700

    added get method, and some func set header
    added get method, and some func set header

--- request_cpp.cpp
+++ request_cpp.cpp
@@ -157,3 +157,381 @@ void request::setTimeout(int timeout)
 {
 	this->timeout = timeout;
 }
+
+
+
+// TO DO: Set some properites headers
+void request::setAcceptHeader(string acceptType)
+{
+	header h;
+	h.name = "accept";
+	h.value = acceptType;
+	this->headers.push_back(h); // add header to the headers vector
+}
+
+
+void request::setContentTypeHeader(string contentType)
+{
+	header h;
+	h.name = "content-type";
+	h.value = contentType;
+	this->headers.push_back(h); // add header to the headers vector
+}
+
+void request::setAuthorizationHeader(string authorization)
+{
+	header h;
+	h.name = "authorization";
+	h.value = authorization;
+}
+
+void request::setAcceptLanguageHeader(string acceptLanguage)
+{
+	header h;
+	h.name = "accept-language";
+	h.value = acceptLanguage;
+	this->headers.push_back(h); // add header to the headers vector
+}
+
+void request::setUserAgentHeader(string userAgent)
+{
+	header h;
+	h.name = "user-agent";
+	h.value = userAgent;
+	this->headers.push_back(h); // add header to the headers vector
+}
+
+void request::setOriginHeader(string origin)
+{
+	header h;
+	h.name = "origin";
+	h.value = origin;
+	this->headers.push_back(h); // add header to the headers vector
+}
+
+void request::setRefererHeader(string referer)
+{
+	header h;
+	h.name = "referer";
+	h.value = referer;
+	this->headers.push_back(h); // add header to the headers vector
+}
+
+void request::setCookieHeader(string cookie)
+{
+	header h;
+	h.name = "cookie";
+	h.value = cookie;
+	this->headers.push_back(h); // add header to the headers vector
+}
+
+void request::setCacheControlHeader(string cacheControl)
+{
+	header h;
+	h.name = "cache-control";
+	h.value = cacheControl;
+	this->headers.push_back(h); // add header to the headers vector
+}
+
+void request::setPragmaHeader(string pragma)
+{
+	header h;
+	h.name = "pragma";
+	h.value = pragma;
+	this->headers.push_back(h); // add header to the headers vector
+}
+
+void request::setConnectionHeader(string connection)
+{
+	header h;
+	h.name = "connection";
+	h.value = connection;
+	this->headers.push_back(h); // add header to the headers vector
+}
+
+void request::setHostHeader(string host)
+{
+	header h;
+	h.name = "host";
+	h.value = host;
+	this->headers.push_back(h); // add header to the headers vector
+}
+
+void request::setUpgradeInsecureRequestsHeader(string upgradeInsecureRequests)
+{
+	header h;
+	h.name = "upgrade-insecure-requests";
+	h.value = upgradeInsecureRequests;
+	this->headers.push_back(h); // add header to the headers vector
+}
+
+void request::setAcceptEncodingHeader(string acceptEncoding)
+{
+	header h;
+	h.name = "accept-encoding";
+	h.value = acceptEncoding;
+	this->headers.push_back(h); // add header to the headers vector
+}
+
+void request::setDntHeader(string dnt)
+{
+	header h;
+	h.name = "dnt";
+	h.value = dnt;
+	this->headers.push_back(h); // add header to the headers vector
+}
+
+
+void request::setTeHeader(string te)
+{
+	header h;
+	h.name = "te";
+	h.value = te;
+	this->headers.push_back(h); // add header to the headers vector
+}
+
+void request::setRangeHeader(string range)
+{
+	header h;
+	h.name = "range";
+	h.value = range;
+	this->headers.push_back(h); // add header to the headers vector
+}
+
+void request::setIfModifiedSinceHeader(string ifModifiedSince)
+{
+	header h;
+	h.name = "if-modified-since";
+	h.value = ifModifiedSince;
+	this->headers.push_back(h); // add header to the headers vector
+}
+
+void request::setIfNoneMatchHeader(string ifNoneMatch)
+{
+	header h;
+	h.name = "if-none-match";
+	h.value = ifNoneMatch;
+	this->headers.push_back(h); // add header to the headers vector
+}
+
+
+void request::setIfRangeHeader(string ifRange)
+{
+	header h;
+	h.name = "if-range";
+	h.value = ifRange;
+	this->headers.push_back(h); // add header to the headers vector
+}
+
+void request::setIfUnmodifiedSinceHeader(string ifUnmodifiedSince)
+{
+	header h;
+	h.name = "if-unmodified-since";
+	h.value = ifUnmodifiedSince;
+	this->headers.push_back(h); // add header to the headers vector
+}
+
+void request::setMaxForwardsHeader(string maxForwards)
+{
+	header h;
+	h.name = "max-forwards";
+	h.value = maxForwards;
+	this->headers.push_back(h); // add header to the headers vector
+}
+
+
+void request::setProxyAuthorizationHeader(string proxyAuthorization)
+{
+	header h;
+	h.name = "proxy-authorization";
+	h.value = proxyAuthorization;
+	this->headers.push_back(h); // add header to the headers vector
+}
+
+
+void request::setAcceptCharsetHeader(string acceptCharset)
+{
+	header h;
+	h.name = "accept-charset";
+	h.value = acceptCharset;
+	this->headers.push_back(h); // add header to the headers vector
+}
+
+void request::setAcceptDatetimeHeader(string acceptDatetime)
+{
+	header h;
+	h.name = "accept-datetime";
+	h.value = acceptDatetime;
+	this->headers.push_back(h); // add header to the headers vector
+}
+
+void request::setAccessControlRequestMethodHeader(string accessControlRequestMethod)
+{
+	header h;
+	h.name = "access-control-request-method";
+	h.value = accessControlRequestMethod;
+	this->headers.push_back(h); // add header to the headers vector
+}
+
+void request::setAccessControlRequestHeadersHeader(string accessControlRequestHeaders)
+{
+	header h;
+	h.name = "access-control-request-headers";
+	h.value = accessControlRequestHeaders;
+	this->headers.push_back(h); // add header to the headers vector
+}
+
+void request::setPriorityHeader(string priority)
+{
+	header h;
+	h.name = "priority";
+	h.value = priority;
+	this->headers.push_back(h); // add header to the headers vector
+}
+
+
+void request::setSecChUaHeader(string secChUa)
+{
+	header h;
+	h.name = "sec-ch-ua";
+	h.value = secChUa;
+	this->headers.push_back(h);
+}
+
+void request::setSecChUaMobileHeader(string secChUaMobile)
+{
+	header h;
+	h.name = "sec-ch-ua-mobile";
+	h.value = secChUaMobile;
+	this->headers.push_back(h);
+}
+
+void request::setSecChUaPlatformHeader(string secChUaPlatform)
+{
+	header h;
+	h.name = "sec-ch-ua-platform";
+	h.value = secChUaPlatform;
+	this->headers.push_back(h);
+}
+
+
+void request::setSecFetchDestHeader(string secFetchDest)
+{
+	header h;
+	h.name = "sec-fetch-dest";
+	h.value = secFetchDest;
+	this->headers.push_back(h);
+}
+
+
+void request::setSecFetchModeHeader(string secFetchMode)
+{
+	header h;
+	h.name = "sec-fetch-mode";
+	h.value = secFetchMode;
+	this->headers.push_back(h);
+}
+
+void request::setSecFetchSiteHeader(string secFetchSite)
+{
+	header h;
+	h.name = "sec-fetch-site";
+	h.value = secFetchSite;
+	this->headers.push_back(h);
+}
+
+void request::setSecFetchUserHeader(string secFetchUser)
+{
+	header h;
+	h.name = "sec-fetch-user";
+	h.value = secFetchUser;
+	this->headers.push_back(h);
+}
+
+// TO DO: Remove header by name
+/*
+	name: string
+	return: true (if removed), false (if not removed)
+	what?: remove header by name
+*/
+bool request::removeHeader(string name)
+{
+	for (int i = 0; i < this->headers.size(); i++)
+	{
+		if (this->headers[i].name == name)
+		{
+			this->headers.erase(this->headers.begin() + i);
+			return true;
+		}
+	}
+
+	return false;
+}
+
+
+// TO DO: Remove headers by vector of names
+/*
+	names: vector of string
+	fall_count_return: boolean (true - return countRemoveFail, false - return countRemoveSuccess)
+	return: int
+	what?: remove headers by vector of names
+*/ 
+int request::removeHeaders(vector<string> names, bool fall_count_return)
+{
+	int countRemoveSuccess = 0, countRemoveFail = 0;
+
+	for (int i = 0; i < names.size(); i++)
+	{
+		if (this->removeHeader(names[i]))
+		{
+			countRemoveSuccess++;
+		}
+		else
+		{
+			countRemoveFail++;
+		}
+	}
+
+	if (fall_count_return) return countRemoveFail; // if fall_count_return is true, return countRemoveFail
+	else return countRemoveSuccess;
+}
+
+// TO DO: View headers
+void request::viewHeaders()
+{
+	cout << "Total headers: " << this->headers.size() << endl;
+
+	for (header h : this->headers)
+	{
+		cout << h.name << ": " << h.value << endl;
+	}
+}
+
+
+// TO DO: Build headers
+string request::buildHeaders()
+{
+	string headers = "";
+	for (header h : this->headers)
+	{
+		headers += "-H \"" + h.name + ": " + h.value + "\" ";
+	}
+	return headers;
+}
+
+
+// TO DO: Get request
+string request::get()
+{
+	if (this->url.empty())
+	{
+		throw needMoreInfoException();
+	}
+	string upperMethod = this->method;
+	transform(upperMethod.begin(), upperMethod.end(), upperMethod.begin(), ::toupper);
+	string command = "curl ";
+	if (this->silent) command += "-s "; // added silent mode
+	if (this->timeout > 0) command += "--max-time " + to_string(this->timeout) + " "; // added timeout
+	command += "-X " + upperMethod + " " + this->url + " " + this->buildHeaders();
+	return this->execToCMD(command); // action curl
+}

544854c3987855dc623d2661d9b61798726ac99b
tronghoa2008 <86024288+hectran12@users.noreply.github.com>
2025-01-14 14:34:44 +0700

    Update main.cpp
    added some test

--- main.cpp
+++ main.cpp
@@ -113,19 +113,118 @@ void test_case_7()
 	outBar(100);
 }
 
+
+void test_case_8()
+{
+	cout << "Test case 8: test headers" << endl;
+	request req("https://google.com", "get");
+	
+
+	// set header with set method
+
+	// type 1 (set header with some set method has been defined)
+	req.setAcceptCharsetHeader("utf-8");
+	req.setAcceptEncodingHeader("gzip, deflate, br");
+	req.setAcceptLanguageHeader("en-US,en;q=0.9,vi;q=0.8");
+	req.setConnectionHeader("keep-alive");
+	req.setHostHeader("google.com");
+	req.setUpgradeInsecureRequestsHeader("1");
+	
+
+	// type 2 (set multi headers with vector<header>)
+	header h;
+	h.name = "priority";
+	h.value = "1";
+
+	header h2;
+	h2.name = "user-agent";
+	h2.value = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0";
+
+	vector<header> headers;
+	headers.push_back(h);
+	headers.push_back(h2);
+
+	req.setHeaders(headers);
+
+
+	// type 3 (set one header)
+	req.setHeader("pragma", "no-cache");
+
+
+
+	// view header method
+
+	cout << "Header beforce remove called" << endl;
+	req.viewHeaders();
+
+	// remove header
+	req.removeHeader("pragma"); // remove pragma header
+	vector<string> names_to_remove = { "priority", "user-agent" };
+	// set false if you want return the count of removed headers (success)
+	// set true if you want return the count of removed headers (fall) 
+	cout << "Total count remove success: " << req.removeHeaders(names_to_remove, false) << endl;
+
+	cout << "Header after remove called" << endl;
+	req.viewHeaders();
+
+
+}
+
+
+void test_case_9()
+{
+	cout << "Test case 9: test build headers";
+	request req("https://google.com", "get");
+	req.setAcceptCharsetHeader("utf-8");
+	req.setAcceptEncodingHeader("gzip, deflate, br");
+	req.setAcceptLanguageHeader("en-US,en;q=0.9,vi;q=0.8");
+	req.setConnectionHeader("keep-alive");
+	req.setHostHeader("google.com");
+	req.setUpgradeInsecureRequestsHeader("1");
+
+	cout << req.buildHeaders() << endl;
+}
+
+
+void test_case_10()
+{
+	cout << "Test case 10: test request (get method)" << endl;
+
+	try
+	{
+		request req("https://parseapi.com/useragent", "get");
+
+		req.setUserAgentHeader("Mozilla/5.0 (iPhone; CPU iPhone OS 17_7_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4.1 Mobile/15E148 Safari/604.1");
+
+		req.setSilent(true);
+
+		cout << req.get() << endl;
+	}
+	catch (exception& e)
+	{
+		cout << e.what() << endl;
+	}
+	
+}
+
 int main()
 {
 
 
-	test_case_1();
-	test_case_2();
-	test_case_3();
-	test_case_4();
-	test_case_5();
-	test_case_6();
-	test_case_7();
-	
+	/*
+		test_case_1();
+		test_case_2();
+		test_case_3();
+		test_case_4();
+		test_case_5();
+		test_case_6();
+		test_case_7();
+	*/
 
+	//test_case_8();
+	//test_case_9();
+
+	test_case_10();
 
 
 
